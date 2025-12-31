8894eb15e37e50dab3fce0c6ad127a083046402d
Avanindra Bose <avanindrabose0218@gmail.com>
2026-01-01 01:45:03 +0530

    Improved Logging and Exception Handling using Middlewares

--- app/core/exception.py
+++ app/core/exception.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI , Request
+import logging
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
-def register_exception_handlers(app:FastAPI):
+logger = logging.getLogger("exception_handler")
+
+def register_exception_handlers(app: FastAPI):
     @app.exception_handler(Exception)
-    async def unhandled_exception_handler(request:Request,exc:Exception):
-        return JSONResponse(status_code=500,content={'detail': str(exc)})
+    async def unhandled_exception_handler(request: Request, exc: Exception):
+        logger.exception(
+            "Unhandled exception | method=%s path=%s",
+            request.method,
+            request.url.path
+        )
+        return JSONResponse(
+            status_code=500,
+            content={"detail": "Internal Server Error"}
+        )
--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from app.api import routes_auth,routes_test
 from app.core.database import engine,Base
+from app.middlewares.response_logger import ResponseLoggerMiddleware
+from app.core.exception import register_exception_handlers
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
-
+app.add_middleware(ResponseLoggerMiddleware)
+register_exception_handlers(app)
 app.include_router(routes_auth.router , tags = ["Auth"])
-app.include_router(routes_test.router,tags = ["Test"])
+app.include_router(routes_test.router,tags = ["Predict"])
